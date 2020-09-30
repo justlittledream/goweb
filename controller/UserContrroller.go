b286fe7fff35f1f61c3a77207fef0359dd750931
justlittledream <1074726545@qq.com>
2020-09-30 17:34:10 +0800

    完成了登录验证

--- controller/UserContrroller.go
+++ controller/UserContrroller.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
-	"github/lhz/ginessential/common"
+	common "github/lhz/ginessential/common"
 	"github/lhz/ginessential/model"
 	util "github/lhz/ginessential/util"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -38,14 +40,62 @@ func Register(ctx *gin.Context) {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "该用户已经存在"})
 		return
 	}
+
+	//创建用户
+	hasedPasswd, err := bcrypt.GenerateFromPassword([]byte(passwd), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密失败"})
+		return
+	}
 	newUser := model.User{
 		Name:     name,
 		Telphone: tel,
-		Passwd:   passwd,
+		Passwd:   string(hasedPasswd),
 	}
 	DB.Create(&newUser)
 	ctx.JSON(200, gin.H{
 		"msg": "注册成功",
+	})
+}
+
+func Login(ctx *gin.Context) {
+	//获取参数
+	DB := common.GetDB()
+	tel := ctx.PostForm("tel")
+	passwd := ctx.PostForm("passwd")
+
+	//数据验证
+	if len(tel) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号长度不对"})
+		return
+	}
+	if len(passwd) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码长度不能少于6位"})
+		return
+	}
+
+	//判断手机号是否存在
+	var user model.User
+	DB.Where("telphone = ?", tel).First(&user)
+	if user.ID == 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		return
+	}
+
+	//判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Passwd), []byte(passwd)); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": 400, "msg": "密码错误"})
+		return
+	}
+
+	//发放token
+	token := "11"
+
+	//返回结果
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"msg":  "登录成功",
+		"data": gin.H{"token": token},
 	})
 }
 
--- routers/routers.go
+++ routers/routers.go
@@ -8,9 +8,13 @@ import (
 )
 
 func CollectRouter(r *gin.Engine) *gin.Engine {
-	r.GET("/api/auth", func(c *gin.Context) {
+	r.GET("/api/auth/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+	r.GET("/api/auth/regis", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "register.html", gin.H{})
 	})
+	r.POST("/api/auth/login", controller.Login)
 	r.POST("/api/auth/register", controller.Register)
 	return r
 }
